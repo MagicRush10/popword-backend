d90a0ed8eceda0666e5ee0dc0a0fe08be254e38a
luca sagliocco <luca.sagliocco@gmail.com>
2025-05-31 13:59:18 +0200

    server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
-app.use('/images', express.static('images'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
