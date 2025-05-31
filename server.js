3c5c77db927ba505e1ec5f1a276afc80e5b61968
luca sagliocco <luca.sagliocco@gmail.com>
2025-05-31 13:29:49 +0200

    server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const questions = [
         id: 'q2',
         type: 'image',
         question: "Da quale film proviene questa immagine?",
-        imageUrl: "https://popword-backend.com/images/star_wars.jpg",
+        imageUrl: "https://popword-backend.com/images/starwars.jpg",
         answer: "Star Wars",
         category: "Cinema",
         hint: "Una saga spaziale epica."
